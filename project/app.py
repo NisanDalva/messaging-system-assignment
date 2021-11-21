0b7acd9b04151a243f61903c6654572a7047bf49
Nisan Dalva <nisandalva@gmail.com>
2021-11-21 17:20:21 +0200

    build models

--- project/app.py
+++ project/app.py
@@ -24,6 +24,43 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 
+@dataclass
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id: int
+    email: str
+    password: str
+    name: str
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(100), unique=True)
+    password = Column(String(100))
+    name = Column(String(100))
+
+
+@dataclass
+class Message_Table(db.Model, Base):
+    __tablename__ = 'message'
+    __table_args__ = (ForeignKeyConstraint(['sender', 'receiver'], ['user.id', 'user.id']),)
+    id: int
+    sender: int
+    receiver: int
+    message: str
+    subject: str
+    creation_date: datetime
+    did_read: bool
+
+    id = Column(Integer, primary_key=True)
+    sender = Column(Integer, ForeignKey('user.id'), nullable=False)
+    receiver = Column(Integer, ForeignKey('user.id'), nullable=False)
+    message = Column(String(200), nullable=False)
+    subject = db.Column(db.String(100), nullable=False)
+    creation_date = Column(DateTime, default=datetime.utcnow)
+    did_read = Column(Boolean, default=False)
+    
+    sender_relationship = relationship("User", foreign_keys=[sender])
+    receiver_relationship = relationship("User", foreign_keys=[receiver]) 
+
 if __name__ == "__main__":
     
     if not os.path.exists(DATABASE_NAME):
